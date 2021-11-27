7dc9e063f49b88bb1d4071363582134f9c4000d3
dtun <dtun@me.com>
2021-11-27 11:08:41 -0700

    Set up keystone ts config

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long should they stay logged in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // todo Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    // todo Change this for roles
+    isAccessAllowed: () => true,
+  },
+  // todo Add session values here
+});
