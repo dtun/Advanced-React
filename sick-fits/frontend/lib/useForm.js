187d3974c2a7bd6f41ffc873299316cc3297b341
dtun <dtun@me.com>
2021-12-01 08:45:15 -0700

    Add useForm hook

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type, files } = e.target;
+    if (type === 'number') {
+      value = Number(value);
+    }
+    if (type === 'file') {
+      value[0] = files;
+    }
+    setInputs({
+      ...inputs, // copy existing state
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
