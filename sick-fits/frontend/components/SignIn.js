396ed6e7c1adc374335a9abc2491849edcfe59ae
dtun <dtun@me.com>
2022-01-12 08:45:38 -0700

    Wire up SignInPage

---
+++ sick-fits/frontend/components/SignIn.js
@@ -0,0 +1,76 @@
+import { useMutation, gql } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+import DisplayError from './ErrorMessage';
+
+export const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          email
+          name
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`;
+
+export function SignIn() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+  const [signIn, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  const error =
+    data?.authenticateUserWithPassword.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signIn();
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
+      <DisplayError error={error} />
+      <fieldset aria-busy={loading}>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Your Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
