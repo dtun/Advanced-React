e14f0811425ab997ed678be3e8469f0b48c61a68
dtun <dtun@me.com>
2022-02-15 08:35:26 -0700

    Add RemoveFromCart

---
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import { CURRENT_USER_QUERY } from './User';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: var(--red);
+    cursor: pointer;
+  }
+`;
+
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default function RemoveFromCart({ id }) {
+  const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+    variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  return (
+    <BigButton
+      disabled={loading}
+      onClick={removeFromCart}
+      title="Remove This Item From Cart"
+      type="button"
+    >
+      &times;
+    </BigButton>
+  );
+}
