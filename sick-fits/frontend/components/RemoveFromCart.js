ea66fb435cd5816c251c9e5237b49ecc63e53545
dtun <dtun@me.com>
2022-02-15 08:41:21 -0700

    Use cache.evict in RemoveFromCart

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
-import { CURRENT_USER_QUERY } from './User';
 
 const BigButton = styled.button`
   font-size: 3rem;
@@ -21,10 +20,20 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    update,
+    // optimisticResponse: {
+    //   deleteCartItem: {
+    //     '--typename': 'CartItem',
+    //     id,
+    //   },
+    // },
   });
   return (
     <BigButton
