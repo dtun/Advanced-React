7111f6398d991774b50d4b7106a654d644d275cc
dtun <dtun@me.com>
2022-03-07 08:39:37 -0700

    Add access

---
+++ sick-fits/backend/access.ts
@@ -0,0 +1,43 @@
+import { ListAccessArgs } from './types';
+import { permissionsList } from './schemas/fields';
+
+// Yes or No value depending on the user's session
+export function isSignedIn({ session }: ListAccessArgs) {
+  return !!session;
+}
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Check if user meets criteria - Yes or No
+export const permissions = {
+  ...generatedPermissions,
+};
+
+// Rule based functions
+// Can return boolean - Yes or No
+// Or filter limiting products they can CRUD
+
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // canManageProducts permission?
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // Is owner of product?
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // Only see available
+    return { status: 'AVAILABLE' };
+  },
+};
