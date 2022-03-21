c3705e42ce08d739a123d4f9976606241d5bbde4
dtun <dtun@me.com>
2022-03-21 08:47:13 -0700

    Add Product test

---
+++ sick-fits/frontend/__tests__/Product.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import Product from '../components/Product';
+import { fakeItem } from '../lib/testUtils';
+
+const product = fakeItem();
+
+describe('Product', () => {
+  it('renders the price tag and title', () => {
+    const { container } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+
+    const priceTag = screen.getByText('$50');
+    expect(priceTag).toBeInTheDocument();
+
+    const link = container.querySelector('a');
+    expect(link).toHaveAttribute('href', '/product/abc123');
+    expect(link).toHaveTextContent(product.name);
+  });
+});
