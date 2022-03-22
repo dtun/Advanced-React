da15546e0b7c0278e8b98c009047358cd2db5b62
dtun <dtun@me.com>
2022-03-22 08:33:17 -0700

    Update test to use snapshot

--- sick-fits/frontend/__tests__/Product.test.js
+++ sick-fits/frontend/__tests__/Product.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import Product from '../components/Product';
 import { fakeItem } from '../lib/testUtils';
@@ -13,11 +13,6 @@ describe('Product', () => {
       </MockedProvider>
     );
 
-    const priceTag = screen.getByText('$50');
-    expect(priceTag).toBeInTheDocument();
-
-    const link = container.querySelector('a');
-    expect(link).toHaveAttribute('href', '/product/abc123');
-    expect(link).toHaveTextContent(product.name);
+    expect(container).toMatchSnapshot();
   });
 });
